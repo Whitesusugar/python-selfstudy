ed794355a9d7118fb9bdcfc9a20408f8ac01c2b6
Aaron <105428400+Whitesusugar@users.noreply.github.com>
2024-09-20 13:54:11 +0800

    Add files via upload

---
+++ 24.9.20 r''.py
@@ -0,0 +1,14 @@
+print('h\nyy') #换行
+print(r'h\nyy') #不换行h\nyy
+
+word = 'Python'
+print(word[0], word[5])
+#P n
+print(word[-1], word[-6])
+#n P
+#与 C 字符串不同的是，Python 字符串不能被改变。向一个索引位置赋值，比如 word[0] = 'm' 会导致错误。
+#反斜杠可以用来转义，使用r可以让反斜杠不发生转义。
+#Python中的字符串有两种索引方式，从左往右以0开始，从右往左以-1开始。
+
+
+
